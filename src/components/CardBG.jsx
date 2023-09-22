ffcf8b3443ddbee61e80dab20f398a5648451c69
Juan Pablo Jimenez <pablojimenez@MM-crowd-326s-Mac-mini.local>
2023-09-22 10:34:05 -0600

    Finished component
    Installed and configurated the color palette of the project, the font and created the Card component

--- src/components/CardBG.jsx
+++ src/components/CardBG.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 export default function CardBG(props) {
   return (
     <div class='container grid grid-rows-1 bg-BC-white p-8 rounded-lg self-center items-center content-center w-auto h-auto'>
-      <div class='grid grid-cols-2'>
-        <img src={props.imgVector} alt='Brightcoders Logo' class='pl-20 pr-10' />
-        <div class='container items-center justify-center self-center '>
-          <div>
-            <text class='text-2xl text-BC-blue font-bold font-body'>{props.title}</text>
+      <div class='flex justify-evenly '>
+        <div class='grid grid-cols-10'>
+          <div class='col-span-3'>
+            <img src={props.imgVector} alt='Brightcoders Logo' />
           </div>
-          <div>
-            <text class='text-2xl text-BC-purple font-body'>{props.subtitle}</text>
+          <div class='self-center col-span-7 pl-4'>
+            <div>
+              <text class='text-2xl text-BC-blue font-bold font-body'>{props.title}</text>
+            </div>
+            <div>
+              <text class='text-2xl text-BC-purple font-body'>{props.subtitle}</text>
+            </div>
           </div>
         </div>
       </div>
-      <div class='grid grid-rows-3 self-center items-center content-center mt-10'>
+      <div class='grid grid-rows-3 self-center items-center content-center mt-3'>
         <div>
           <text class='text-sm font-body'>{props.textContent}</text>
         </div>
